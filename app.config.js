4f8de69f206a74aa120c010131b0459436a0c861
abaelhacan-spec <abaelhacan@gmail.com>
2026-06-06 22:12:22 +0100

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -3,27 +3,31 @@ module.exports = {
     name: "مصاريف",
     slug: "expense-tracker",
     version: "1.0.0",
-    owner: "abaelhacan",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "expense-tracker",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
+
     splash: {
-      "image": "./assets/images/icon.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#0B1120"
+      image: "./assets/images/icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#0B1120"
     },
+
     ios: {
       supportsTablet: false
     },
+
     android: {
       package: "com.hussein.masareef",
       permissions: ["NOTIFICATIONS"]
     },
+
     web: {
       favicon: "./assets/images/icon.png"
     },
+
     plugins: [
       ["expo-router", { origin: "https://replit.com/" }],
       "expo-font",
@@ -36,14 +40,13 @@ module.exports = {
         }
       ]
     ],
+
     experiments: {
-      typedRoutes: true,
-      reactCompiler: true
+      typedRoutes: true
     },
+
     extra: {
-      eas: {
-        projectId: "ebc03e4c-aff6-4d3a-baa4-0a71031c14dd"
-      }
+      eas: {}
     }
   }
 };
